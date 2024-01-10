d776e2410a3398eb0e7c498d4b1661eb0547d3b8
dato <chechelashvili.dato111@gmail.com>
2024-01-10 23:58:39 +0400

    fixed bug

--- src/views/auth/sendingView.vue
+++ src/views/auth/sendingView.vue
@@ -28,7 +28,7 @@
                             <div class="input-group mb-3">
                                 <input type="email" class="form-control input_form_add" v-model="newEmail">
                                 <button type="button" class="btn btn-secondary" @click="addEmail()">
-                                    <svg xmlns="http://www.w3.org/2000/svg" width="18" height="18" fill="currentColor"    class="bi bi-person-plus" viewBox="0 0 16 16">
+                                    <svg xmlns="http://www.w3.org/2000/svg" width="18" height="18" fill="currentColor" class="bi bi-person-plus" viewBox="0 0 16 16">
                                         <path d="M6 8a3 3 0 1 0 0-6 3 3 0 0 0 0 6m2-3a2 2 0 1 1-4 0 2 2 0 0 1 4 0m4 8c0 1-1 1-1 1H1s-1 0-1-1 1-4 6-4 6 3 6 4m-1-.004c-.001-.246-.154-.986-.832-1.664C9.516 10.68 8.289 10 6 10s-3.516.68-4.168 1.332c-.678.678-.83 1.418-.832 1.664z"/>
                                         <path fill-rule="evenodd" d="M13.5 5a.5.5 0 0 1 .5.5V7h1.5a.5.5 0 0 1 0 1H14v1.5a.5.5 0 0 1-1 0V8h-1.5a.5.5 0 0 1 0-1H13V5.5a.5.5 0 0 1 .5-.5"/>
                                     </svg>
@@ -39,7 +39,7 @@
                                 <ol>
                                     <li  v-for="(email, index) in emails" :key="index">
                                         {{ email }}
-                                        <button class="btn btn_delete" @click="deleteEmail(index)">
+                                        <button class="btn btn_delete" type="button" @click="deleteEmail(index)">
                                             <svg xmlns="http://www.w3.org/2000/svg" width="25" height="25" fill="currentColor" class="bi bi-x delete_icon" viewBox="0 0 16 16">
                                             <path d="M4.646 4.646a.5.5 0 0 1 .708 0L8 7.293l2.646-2.647a.5.5 0 0 1 .708.708L8.707 8l2.647 2.646a.5.5 0 0 1-.708.708L8 8.707l-2.646 2.647a.5.5 0 0 1-.708-.708L7.293 8 4.646 5.354a.5.5 0 0 1 0-.708"/>
                                             </svg>

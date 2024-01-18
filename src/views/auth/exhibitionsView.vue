5fbb956b86a362eede044acc09e9bd4b5aa0ec6a
= <=>
2024-01-18 14:58:54 +0400

    modified

--- src/views/auth/exhibitionsView.vue
+++ src/views/auth/exhibitionsView.vue
@@ -59,17 +59,17 @@
             <div class="modal fade" id="confirmationModal">
                 <div class="modal-dialog">
                     <div class="modal-content">
-                    <div class="modal-header">
-                        <h5 class="modal-title">გთხოვთ დაადასტუროთ</h5>
-                        <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
-                    </div>
-                    <div class="modal-body">
-                        ნამდვილად გსურთ მოცემული გამოფენის წაშლა?
-                    </div>
-                    <div class="modal-footer">
-                        <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">უარყოფა</button>
-                        <button type="button" class="btn btn-danger" data-bs-dismiss="modal" @click="deleteExhibition">წაშლა</button>
-                    </div>
+                        <div class="modal-header">
+                            <h5 class="modal-title">გთხოვთ დაადასტუროთ</h5>
+                            <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+                        </div>
+                        <div class="modal-body">
+                            ნამდვილად გსურთ მოცემული გამოფენის წაშლა?
+                        </div>
+                        <div class="modal-footer">
+                            <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">უარყოფა</button>
+                            <button type="button" class="btn btn-danger" data-bs-dismiss="modal" @click="deleteExhibition">წაშლა</button>
+                        </div>
                     </div>
                 </div>
             </div>
